0e92b326b6d118dbc57b6d8d6846b8cd4d709ec8
Anoop Dwivedi <anoopdwivedi816@gmail.com>
2026-08-04 19:33:24 +0530

    langchain-chains completed

---
+++ Langchain-Chains/conditional_chain.py
@@ -0,0 +1,63 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel,  RunnableBranch, RunnableLambda
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import Literal
+
+load_dotenv()
+
+
+model = ChatGroq(
+    model = 'llama-3.1-8b-instant'
+)
+
+parser = StrOutputParser()
+
+class Feedback(BaseModel):
+    sentiment: Literal['positive','negative']=Field(description='Give the sentiment of the feedback')
+
+parser2 = PydanticOutputParser(pydantic_object=Feedback)
+
+prompt1 = PromptTemplate(
+    template='Classify the sentiment of the following feedback text into positive or negative \n {feedback} \n {format_instruction}',
+    input_variables=['feedback'],
+    partial_variables={'format_instruction':parser2.get_format_instructions()}
+)
+
+classifier_chain = prompt1 | model | parser2
+
+#result = classifier_chain.invoke({'feedback':'This is a terrible smartphone'}).sentiment
+
+#rint(result)
+
+prompt2 = PromptTemplate(
+    template='Write an appropriat response to this positive feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+prompt3 = PromptTemplate(
+    template='Write an appropriat response to this negative feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+# Syntax
+'''branch_chain = RunnableBranch(
+    (condition1,chain),
+    (condition2,chian),
+    default chain
+)'''
+
+branch_chain = RunnableBranch(
+    (lambda x:x.sentiment=='positive',prompt2 | model | parser),
+    (lambda x:x.sentiment=='negative', prompt3 | model | parser),
+    RunnableLambda(lambda x: 'could not find sentiment')
+)
+
+chain = classifier_chain | branch_chain
+
+print(chain.invoke({'feedback':'This is a terrible phone'}))
+
+print(chain.get_graph().print_ascii())
