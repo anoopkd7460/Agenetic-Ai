df61317687edc5a911de91fe490ff8a32875a69a
AnoopDwivedi <anoopdwivedi816@gmail.com>
2026-08-08 13:33:38 +0530

    Add Langchain Runnables

---
+++ Langchain-Runnables/runnable_lambda.py
@@ -0,0 +1,38 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence, RunnableParallel,RunnablePassthrough,RunnableLambda
+
+load_dotenv()
+
+def word_count(text):
+    return len(text.split())
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatGroq(
+    model = 'llama-3.1-8b-instant'
+)
+
+parser = StrOutputParser()
+
+joke_gen_chain = RunnableSequence(prompt1,model,parser)
+
+parallel_chain = RunnableParallel({
+    'joke':RunnablePassthrough(),
+    'word_count':RunnableLambda(word_count)
+})
+
+final_chain = RunnableSequence(joke_gen_chain,parallel_chain)
+
+# print(final_chain.invoke({'topic':'AI'}))
+
+result = final_chain.invoke({'topic':'AI'})
+
+final_result = '''{} \n word count - {}'''.format(result['joke'], result['word_count'])
+
+print(final_result)
